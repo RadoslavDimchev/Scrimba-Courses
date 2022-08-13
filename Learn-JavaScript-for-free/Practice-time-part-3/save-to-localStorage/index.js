3397d514123e9007113cf99ae09ea1af7fb8b0b7
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2022-08-13 16:24:14 +0300

    Added tasks from last practice time

---
+++ Learn-JavaScript-for-free/Practice-time-part-3/save-to-localStorage/index.js
@@ -0,0 +1,9 @@
+// Save a value to localStorage
+// Delete your code and refresh the page
+// Fetch your value from localStorage and log it out
+
+// localStorage.setItem("name", "Radoslav");
+const name = localStorage.getItem("name");
+console.log(name);
+
+// works in browser console
